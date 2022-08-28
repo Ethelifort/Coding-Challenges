d8f274cbedbf8b62a451449b7eb526a87fa4d5e2
Ernst Thelifort <ethelifort@gmail.com>
2022-08-27 21:31:58 -0400

    New 7kyu completed, String Method concat

---
+++ 7kyu/indexedcharacters.js
@@ -0,0 +1,30 @@
+/* Problem
+Given a string s. You have to return another string such that even-indexed
+and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+*/
+
+
+function sortMyString(S) {
+
+    let evenWords = '';
+    let oddWords = ''; 
+  
+    for(i = 0;i<S.length;i++){
+  
+       if(i % 2 ==0){
+        evenWords = evenWords + S[i];
+       }
+  
+       else {
+        oddWords = oddWords + S[i];
+       }
+  
+    }
+    return evenWords.concat(' ', oddWords);
+  }
+  
+  
+  
+  
+  //Tests
+  console.log(sortMyString("YCOLUE'VREER"));   
