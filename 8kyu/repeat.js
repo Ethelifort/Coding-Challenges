d8f059884ae6c601f55029ce6efede2941c31cff
Ernst Thelifort <ethelifort@gmail.com>
2022-08-28 22:29:53 -0400

    8kyu completed

---
+++ 8kyu/repeat.js
@@ -0,0 +1,11 @@
+/* Problem
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+*/
+
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+  
+  //Tests
+  console.log(repeatStr(5,"s"));
