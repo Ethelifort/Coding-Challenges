61bf9786cee7449e24e414aecefb3a1f0ea8a2b6
Ernst Thelifort <ethelifort@gmail.com>
2022-09-01 23:57:58 -0400

    7kyu completed

---
+++ 7kyu/oddoreven.js
@@ -0,0 +1,28 @@
+/* Problem
+
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+*/
+
+function oddOrEven(array) {
+
+  
+    if(array.length == 0 ){
+      return "even";
+    
+    }
+  
+    let total = array.reduce((a,b)=>a+b);
+  
+    if(total % 2 == 0){
+    return "even";
+    } 
+    else{
+      return "odd"; }
+  }
+  
+  //Tests
+  
+  oddOrEven([0, 2, 4]);
+  
