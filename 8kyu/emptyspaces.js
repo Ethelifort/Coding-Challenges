0e047ca2f0db28e9e04bd5b449f0c63e8c63777f
Ernst Thelifort <ethelifort@gmail.com>
2022-09-02 23:39:12 -0400

    8kyu completed

---
+++ 8kyu/emptyspaces.js
@@ -0,0 +1,11 @@
+/* Problem
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+function noSpace(x){
+    let string =  x.split(" ");
+    return string.join("")
+  }
+  
+  //Tests
+  noSpace('8 j 8   mBliB8g  imjB8B8  jl  B');
