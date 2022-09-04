a1e054c21a4c9b22b48b3f25d12b0fe79a4320a0
Ernst Thelifort <ethelifort@gmail.com>
2022-09-03 22:07:24 -0400

    8kyu completed

---
+++ 8kyu/countingsheep.js
@@ -0,0 +1,24 @@
+/* Problem
+Consider an array/list of sheep where some sheep may be missing from their place.
+We need a function that counts the number of sheep present in the array (true means present).*/
+
+let array1 = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ];
+  
+
+  function countSheeps(arrayOfSheep) {
+    let counter = 0;
+  for(i = 0; i<arrayOfSheep.length;i++){
+      if(arrayOfSheep[i] == true){
+        counter++;
+      }
+  }
+  return counter;
+  }
+  
+  //Tests
+  countSheeps(array1);
--- 8kyu/emptyspaces.js
+++ 8kyu/emptyspaces.js
@@ -1,5 +1,7 @@
 /* Problem
 Simple, remove the spaces from the string, then return the resultant string.
+
+
 */
 
 function noSpace(x){
