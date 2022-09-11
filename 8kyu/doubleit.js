7499e6f63af57c4c10681a2e8e66e8e14a2f4c25
Ernst Thelifort <ethelifort@gmail.com>
2022-09-10 23:54:25 -0400

    8kyu completed

---
+++ 8kyu/doubleit.js
@@ -0,0 +1,9 @@
+/* Problem
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i) {
+    return i * 2;
+  }
+//Tests
+console.log(doubleInteger(2), 4);
