b5d03764e718ad4f7fb88933ffd529ca698fe586
Ernst Thelifort <ethelifort@gmail.com>
2022-09-12 23:31:45 -0400

    8kyu completed

---
+++ 8kyu/booleantostring.js
@@ -0,0 +1,9 @@
+/* Problem
+Implement a function which convert the given boolean value into its string representation.
+*/
+function booleanToString(b){
+    return b.toString();
+    }
+      
+    //Tests
+    console.log(booleanToString(true));
