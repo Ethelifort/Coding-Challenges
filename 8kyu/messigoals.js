ffc4a3665934984daa1c4b3f6b36b7ff0a0b235a
Ernst Thelifort <ethelifort@gmail.com>
2022-09-17 23:24:21 -0400

    8yu completed

---
+++ 8kyu/messigoals.js
@@ -0,0 +1,8 @@
+/* Problem
+Complete the function to return his total number of goals in all three leagues.
+*/
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+  //Tests
+  console.log(goals(43, 10, 5), 58);
