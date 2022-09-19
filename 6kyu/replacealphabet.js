20abadd977eae6a244bda65f763a0638a8a21b0f
Ernst Thelifort <ethelifort@gmail.com>
2022-09-19 00:32:27 -0400

    6kyu completed

--- 6kyu/replacealphabet.js
+++ 6kyu/replacealphabet.js
@@ -3,19 +3,24 @@ In this kata you are required to, given a string, replace every letter with its 
 If anything in the text isn't a letter, ignore it and don't return it.*/
 
 function alphabetPosition(text) {
-    let  letters = String.fromCharCode(97);
+    let  letters = String.fromCharCode(97);// Get the letter a and push 
     let arr = [];
+    let textIndex = [];
     let lowerCaseArr =  text.toLowerCase();
-    console.log(lowerCaseArr);
   
-    for(i = 0; i<26;i++){
+    for(i = 0; i<26;i++){  
       arr.push(letters);
-      letters =  String.fromCharCode(98 + i);
+      letters =  String.fromCharCode(97 + i);
+    }
+    for(j = 0; j<lowerCaseArr.length;j++){
+      for(k = 0;k<=arr.length-1;k++){
+        if(lowerCaseArr[j] ==  arr[k]){
+          textIndex.push(arr.indexOf(arr[k]));
+        }
+    }    
     }
   
-    for(j = 0; j<lowerCaseArr.length;j++){
-      
-    }
+    return textIndex.join(" ")
   
   }
   //Tests
