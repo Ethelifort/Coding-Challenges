2119ffb4f5f84218fe25243f9d144ff8464fa46c
Ernst Thelifort <ethelifort@gmail.com>
2022-09-26 19:20:56 -0400

    8kyu completed/refactored

--- 8kyu/isogram.js
+++ 8kyu/isogram.js
@@ -5,19 +5,27 @@ Implement a function that determines whether a string that contains only letters
 
 function isIsogram(str){
 
-    let arr = [];
-      let strLowercaseArr = str.split("");
-      let strLowercase = strLowercaseArr.map((a)=>a.toLowerCase());
-      console.log(strLowercase)
-    
-      for(i = 0; i<strLowercaseArr.length;i++){
-        for(j = 1; j <str.length; j++){
-          if(strLowercaseArr[i] == str[j]){
-            console.log("Test")
-          }
+  let arr = [];
+    let strLowercaseArr = str.split("");
+    let strLowercase = strLowercaseArr.map((a)=>a.toLowerCase());
+    let counter = 0;
+  
+    for(i = 0; i<strLowercase.length;i++){
+      arr.push(strLowercase[i]); //Push first letter into arr.
+      for(j = 0; j < arr.length;j++){
+        if(strLowercase[i+1] == arr[j]){ //Check the first letter with letters in StrlowerCase starting from the seocnd letter.
+          counter++;  
         }
       }
-    
-    //Tests
     }
-    console.log(isIsogram("moOse"))
+  
+    let result = counter>0? false :  true;
+    return result;
+  }
+  
+  
+  
+  
+  //Tests
+  
+  console.log(isIsogram("moose"))
