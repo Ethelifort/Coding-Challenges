f3b1b7f9fcd333c7b3bd21175e3974b1b05a639d
Ernst Thelifort <ethelifort@gmail.com>
2022-10-07 23:55:23 -0400

    8kyu completed

---
+++ 8kyu/squareorrectangle.js
@@ -0,0 +1,16 @@
+/* Problem
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+*/
+
+
+const areaOrPerimeter = function(l , w) {
+    if(l == w){
+      return l * w;
+    } else {
+      return l * 2 + w * 2;
+    }
+    };
+  //Tests
+  
+  console.log(areaOrPerimeter(6 , 10) , 32)
