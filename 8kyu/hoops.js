c74d8f9a7b4fe4989d869a7b3cca851816bc2488
Ernst Thelifort <ethelifort@gmail.com>
2022-10-10 23:46:51 -0400

    8kyu completed

---
+++ 8kyu/hoops.js
@@ -0,0 +1,17 @@
+/* Problem
+Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+*/
+let countBits = function(n) {
+    let numOfOnes = 0;
+    let binaryNum = n.toString(2);
+    
+    function hoopCount (n) {
+      let total = n >= 10? "Great, now move on to tricks": "Keep at it until you get it"
+      return total;
+    }
+    //Tests
+    }
+    console.log(hoopCount(5));
