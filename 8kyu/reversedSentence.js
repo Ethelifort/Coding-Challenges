b7c484ce8a05143dbd76951d29c9e891c84ac28a
Ernst Thelifort <ethelifort@gmail.com>
2022-10-13 23:15:55 -0400

    8kyu completed

---
+++ 8kyu/reversedSentence.js
@@ -0,0 +1,11 @@
+/* Problem
+Complete the solution so that it reverses all of the words within the string passed in.
+*/
+
+// complete the function
+function reverseWords(str){
+    return str.split(" ").reverse().join(" ")
+    }
+  
+  //Tests
+  console.log("Ernst is the greatest ever")
