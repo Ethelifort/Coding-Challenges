e25b83830bd584dd70b866ea1ef2228a8bdb849e
Ernst Thelifort <ethelifort@gmail.com>
2022-10-16 23:51:38 -0400

    8kyu completed

---
+++ 8kyu/typeof.js
@@ -0,0 +1,11 @@
+/* Problem
+Return the type of the sum of the two arguments
+*/
+
+// complete the function
+function typeOfSum(a, b) {
+    return typeof(a + b);
+  }
+  //Tests
+  
+  console.log(typeOfSum(5,5))
