9340b4b43290e31517e5ff12692c3c8d2b1f12fe
Ernst Thelifort <ethelifort@gmail.com>
2022-11-30 23:54:20 -0500

    8kyu , fundamentals

---
+++ 8kyu/divisibleby.js
@@ -0,0 +1,16 @@
+/* Problem
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor. */
+
+function divisibleBy(numbers, divisor){
+    let arr = [];
+    for(i = 0; i<numbers.length;i++){
+  
+      if(numbers[i] % divisor == 0){
+        arr.push(numbers[i]);
+      }
+    }
+    return arr;    
+    }
+    
+  //Tests
+  console.log(divisibleBy([1,2,3,4,5,6], 2));
