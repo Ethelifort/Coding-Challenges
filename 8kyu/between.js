b91ecd3d5dbe3288df6d5a2973fe7023a08f9d10
Ernst Thelifort <ethelifort@gmail.com>
2022-12-02 23:18:28 -0500

    8kyu completed algo,fundamentals

---
+++ 8kyu/between.js
@@ -0,0 +1,20 @@
+/* Problem
+Complete the function that takes two integers (a, b, where a < b)
+ and return an array of all integers between the input parameters, including them.
+*/
+
+function between(a, b) {
+  
+    let arr = [];
+      
+    for(i = a; i <=b; i++){
+      arr.push(i);
+    }
+      return arr;
+    }
+  
+  //Tests
+  console.log(between(1,4));
+  console.log(between(1,5));
+  console.log(between(4,8));
+  console.log(between(10,15));
