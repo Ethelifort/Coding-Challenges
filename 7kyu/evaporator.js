d7d815d975a33079acc06b7f4fdc5408a410779a
Ernst Thelifort <ethelifort@gmail.com>
2023-02-07 23:56:18 -0500

    7kyu fundamentals

---
+++ 7kyu/evaporator.js
@@ -0,0 +1,23 @@
+/* Problem
+This program tests the life of an evaporator containing a gas.
+
+We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+
+The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+*/
+function evaporator(content, evap_per_day, threshold){ 
+    let remaining = content;
+     let days = 0;
+     
+     while (remaining / content >= threshold / 100) {
+       
+       days++;
+       remaining -= remaining * evap_per_day / 100;
+     }
+     
+     return days;
+   }
+//Tests
+
+console.log((evaporator(10,10,10),22));
