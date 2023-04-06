c9255d539d6cacb1b2fe1bcf2e6e8392adcf9d38
Ernst Thelifort <ethelifort@gmail.com>
2023-04-06 14:46:43 -0400

    7kyu string completed

--- 7kyu/reversedwords.js
+++ 7kyu/reversedwords.js
@@ -1,20 +1,15 @@
 /* Problem
 Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
-*/
-function reverseWords(str) {
+ /* Notes
+ */
 
-    let arr = [];
-    let strArray = str.split(" ");
-  
-    for(i = 0;i<strArray.length;i++){
-      
-      let test = strArray[i];
-      console.log(test.reverse())
-  
-    }
-  
-  
-  
-  }
+ function reverseWords(str) {
+  let words = str.split(" ");
+  let reversedWords = words.map((word) => {
+    return word.split("").reverse();
+  });
+  return reversedWords.join(" ");
+}
   //Tests
-  console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))
+
+  console.log(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
