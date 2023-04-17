8f2073166ebde0579854d54966a17373ccbfedfb
Ernst Thelifort <ethelifort@gmail.com>
2023-04-16 23:55:10 -0400

    8kyu fundamentals completed

---
+++ 8kyu/squarefunction.js
@@ -0,0 +1,17 @@
+
+/* Problem
+Now you have to write a function that takes an argument and returns the square of it.
+ /* Notes
+ */
+
+
+ function squareNumber(num) {
+    return Math.pow(num, 2);
+  }
+  
+  
+  //Test
+  
+  
+  
+  console.log(squareNumber(2),"4");
