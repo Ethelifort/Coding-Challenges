f8a8341c0d041e333fe7399d028bea8422c3b0c5
Ernst Thelifort <ethelifort@gmail.com>
2023-06-19 15:44:19 -0400

    8kyu debugging completed

---
+++ 8kyu/debugifelse.js
@@ -0,0 +1,18 @@
+/* Problem
+checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+
+The function receives one parameter health which will always be a whole number between -10 and 10.
+
+
+*/
+
+function checkAlive (health) {
+  if (health > 0) {
+    return true;
+  } else{
+    return false;
+  }
+}
+
+
+console.log(checkAlive(6));
